f061add6c6393a619068589bf436a442b11fd43f
jazz405 <jazz405@gmail.com>
2021-04-06 23:10:33 -0400

    test in the air

--- Semaphore/src/main.ts
+++ Semaphore/src/main.ts
@@ -2,11 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as fs from 'fs';
 
 declare const module: any;
+const httpsOptions = {
+  key: fs.readFileSync('./secrets/private-key.pem'),
+  cert: fs.readFileSync('./secrets/public-certificate.pem'),
+};
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    httpsOptions,
     cors: true,
   });
   app.useWebSocketAdapter(new IoAdapter(app));
